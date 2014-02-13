8ebe797ffca6b7a64dd0a3b5cf81d3ae31d08944
PaulieC <counci70@students.rowan.edu>
2014-02-13 12:03:02 -0500

    test commit
    Pushing this project to the github server

---
+++ src/BruteForceSearch.java
@@ -0,0 +1,27 @@
+
+/**
+ * 
+ */
+
+/**
+ * @author PC
+ *
+ */
+class BruteForceSearch {
+
+	/**
+	 * 
+	 */
+	public BruteForceSearch() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
